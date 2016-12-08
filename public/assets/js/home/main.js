ce9acb825df2ca291e86dadc026a51e5497a9410
Diego Lima <diegoleema@gmail.com>
2016-12-08 17:56:06 -0300

    modificacao de slideshow para carousel

--- public/assets/js/home/main.js
+++ public/assets/js/home/main.js
@@ -1,22 +1,12 @@
-	function carregarSlides() {
-		$("#slide img:eq(0)").addClass("ativo").show();
-		var texto = $(".ativo").attr("data-description");
-		$("#slide").prepend("<span>"+texto+"</span>");
-		$("#slide span").hide().html(texto).delay(500).fadeIn();
+$('#carousel').carousel({
+  interval: 2000
+});
 
-		setInterval(slide,4000);
-
-		function slide(){
-
-			if($(".ativo").next().size()){							
-				$(".ativo").fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
-			}else{
-				$(".ativo").fadeOut().removeClass("ativo");
-				$("#slide img:eq(0)").fadeIn().addClass("ativo");
-			}
-
-			$('#slide span').remove();
-			texto = $(".ativo").attr("data-description");
-			$("#slide").prepend("<span>"+texto+"</span>");
-		}
-	}
+$(document).ready(function () {
+	$(".carousel-inner").swiperight(function () {
+		$(this).parent().carousel('prev');
+	});
+	$(".carousel-inner").swipeleft(function () {
+		$(this).parent().carousel('next');
+	});
+});
